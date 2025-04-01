642961d6c36e783784e056bed17bf5f1ae6fe4c2
kevin966 <kevinchen0165@gmail.com>
2025-04-01 09:10:01 +0200

    Add API keys back

--- models_api.py
+++ models_api.py
@@ -1,7 +1,8 @@
 import requests
 import re
 
-API_KEY = ""
+# API_KEY = "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" # Tibi's API key
+API_KEY = "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" # Kevin's API key
 API_URL_MISTRAL = "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.3" # type = chat model
 API_URL_GOOGLE = "https://api-inference.huggingface.co/models/google/flan-t5-small" # type = pretrained
 headers = {"Authorization": f"Bearer {API_KEY}"}
